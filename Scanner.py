3128bc25ea29b4868e119e0624c4af3c4b01ccce
ProstoDurachok <128019923+ProstoDurachok@users.noreply.github.com>
2025-12-18 21:28:27 +0300

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -35,7 +35,7 @@ LOCAL_DB = "items.json"
 APPID = 730
 # Telegram
 TOKEN = os.environ.get("TELEGRAM_TOKEN", "8427688497:AAGkBisiTfJM3RDc8DOG9Kx9l9EnekoFGQk")
-CHAT_ID = os.environ.get("CHAT_ID", "-1003215068555")
+CHAT_ID = os.environ.get("CHAT_ID", "-1003143360650")
 MONITOR_BOT_CHAT_ID = os.environ.get("MONITOR_BOT_CHAT_ID", "873939087") # ID приватного чата с монитор-ботом (опционально)
 # Steam sessionid
 SESSIONID = os.environ.get("STEAM_SESSIONID", None)
@@ -44,7 +44,7 @@ USE_PROXY_BY_DEFAULT = False # Изменено на False для обхода �
 PROXY_HTTP_URL = "http://mm4pkP:a6K4yx@95.181.155.167:8000"
 PROXY_HTTP_ALT = "http://lte6:LVxqnyQiMH@65.109.79.15:13014"
 # Поведение запросов (увеличено для снижения 429)
-REQUEST_DELAY = 20.5 # Увеличено до 7s для снижения 429
+REQUEST_DELAY = 19.5 # Увеличено до 7s для снижения 429
 JITTER = 8.0
 MAX_RETRIES = 2
 MAX_RETRIES_429 = 3
@@ -52,10 +52,10 @@ BACKOFF_BASE = 2.0
 RATE_LIMIT_PAUSE = 30
 RATE_LIMIT_COUNT = 0
 # Фильтры
-VOLATILITY_THRESHOLD = 8.0
-PRICE_CHANGE_THRESHOLD = 10.0
-BREAKOUT_THRESHOLD = 2.0
-MIN_PRICE = 2.0
+VOLATILITY_THRESHOLD = 6.0
+PRICE_CHANGE_THRESHOLD = 8.0
+BREAKOUT_THRESHOLD = 1.5
+MIN_PRICE = 1.25
 MIN_VOLUME_24H = 1
 HISTORY_DAYS = 7
 USD_RATE = 83.4
